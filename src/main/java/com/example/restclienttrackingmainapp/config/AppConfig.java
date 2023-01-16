4f7e2bf4810b2692218bfd0029a0e2b1ab87182e
Daniel <danielworonowicz.it@gmail.com>
2023-01-16 23:34:23 +0100

    Small change in AppConfig

--- src/main/java/com/example/restclienttrackingmainapp/config/AppConfig.java
+++ src/main/java/com/example/restclienttrackingmainapp/config/AppConfig.java
@@ -1,23 +1,22 @@
 package com.example.restclienttrackingmainapp.config;
 
 import com.example.restclienttrackingmainapp.logger.EventLogLogger;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig implements WebMvcConfigurer {
 
     private final EventLogLogger eventLogLogger;
-
-    public AppConfig(EventLogLogger eventLogLogger) {
-        this.eventLogLogger = eventLogLogger;
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(eventLogLogger).excludePathPatterns("/proxy/**");
         registry.addInterceptor(eventLogLogger).addPathPatterns("/trainers/**");
     }
+
 
 }
