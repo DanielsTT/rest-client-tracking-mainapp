42a243ff227bdc0b2c767e9ab80616416a5c3aca
Daniel <danielworonowicz.it@gmail.com>
2023-01-16 23:48:57 +0100

    Enable CSRF

--- src/main/java/com/example/restclienttrackingmainapp/config/SecurityConfig.java
+++ src/main/java/com/example/restclienttrackingmainapp/config/SecurityConfig.java
@@ -17,7 +17,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/css/**", "/bootstrap/**", "/assets/bootstrap-solid.svg",
                         HOME_PAGE,
