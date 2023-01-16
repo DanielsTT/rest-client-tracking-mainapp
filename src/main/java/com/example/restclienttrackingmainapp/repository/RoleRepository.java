e80390e8180742381cc7f63000934889536ebf57
Daniel <danielworonowicz.it@gmail.com>
2023-01-17 00:26:16 +0100

    Add role findByName

--- src/main/java/com/example/restclienttrackingmainapp/repository/RoleRepository.java
+++ src/main/java/com/example/restclienttrackingmainapp/repository/RoleRepository.java
@@ -5,5 +5,7 @@ import com.example.restclienttrackingmainapp.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Role findByName(String name);
 }
 
