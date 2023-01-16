41bb9909d9276fd8a31c0ef36a348f43e6a232b2
Daniel <danielworonowicz.it@gmail.com>
2023-01-16 22:14:17 +0100

    Add TestContainers

---
+++ src/test/java/com/example/restclienttrackingmainapp/AbstractConfigTest.java
@@ -0,0 +1,39 @@
+package com.example.restclienttrackingmainapp;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = AbstractConfigTest.DockerMysqlDataSourceInitializer.class)
+@Testcontainers
+public abstract class AbstractConfigTest {
+
+
+    public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.30-oracle");
+
+    static {
+        mySQLContainer.start();
+    }
+
+    public static class DockerMysqlDataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword()
+            );
+        }
+    }
+}
---
+++ src/test/java/com/example/restclienttrackingmainapp/OneTest.java
@@ -0,0 +1,13 @@
+package com.example.restclienttrackingmainapp;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OneTest extends AbstractConfigTest {
+
+    @Test
+    void testOne(){
+        Assertions.assertEquals(1,1);
+    }
+}
