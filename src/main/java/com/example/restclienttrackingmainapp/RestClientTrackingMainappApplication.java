b634e450990d483a06a06f45463672a7c1b39dc5
Daniel <danielworonowicz.it@gmail.com>
2023-01-16 21:12:32 +0100

    Add ModelMapper

---
+++ src/main/java/com/example/restclienttrackingmainapp/RestClientTrackingMainappApplication.java
@@ -0,0 +1,13 @@
+package com.example.restclienttrackingmainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientTrackingMainappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientTrackingMainappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restclienttrackingmainapp/RestClientTrackingMainappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restclienttrackingmainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientTrackingMainappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
